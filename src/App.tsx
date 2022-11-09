e8629906273033201632dc27b2299e8079197d42
Ol4ik88 <99606360+Ol4ik88@users.noreply.github.com>
2022-11-09 21:34:21 +0300

    RSS-1: init project, customize configuration files (#1)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Start project.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
