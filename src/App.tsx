20a7e770838d43362d71fd95106f768ba85626c9
yolf <gleoros@gmail.com>
2022-11-10 16:40:17 +0300

    RSS-4: add react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Start project.</p>
+        <Container className="text-center">
+          <h1>Start project.</h1>
+        </Container>
       </header>
     </div>
   );
