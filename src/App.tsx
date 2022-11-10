819ec584a8e7bd7bc6b54643ec1696735fd9bd91
rincewizz <95530503+rincewizz@users.noreply.github.com>
2022-11-10 21:27:04 +0300

    RSS-14: configure localization (#4)
    * RSS-4: add react-bootstrap
    
    * RSS-14: configure localization

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import './i18n/config';
+import { useTranslation } from 'react-i18next';
 
-function App() {
+export default function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <Container className="text-center">
-          <h1>Start project.</h1>
+          <h1 onClick={() => changeLanguage('ru')}>Start project.</h1>
+          <p>{t('title')}</p>
         </Container>
       </header>
     </div>
   );
 }
-
-export default App;
