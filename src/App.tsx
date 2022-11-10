00f1454718359cfefd2b052788432b7a8e1d3938
Ol4ik88 <slapikol4ik@gmail.com>
2022-11-10 20:53:18 +0300

    RSS-6: create routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import PrivateRoute from 'components/routing/PrivateRoute';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -6,6 +8,36 @@ function App() {
       <header className="App-header">
         <p>Start project.</p>
       </header>
+      <Routes>
+        <Route path="/" element={<p>Welcome page</p>} />
+        <Route path="/login" element={<p>Sign in</p>} />
+        <Route path="/registration" element={<p>Sign up</p>} />
+        <Route
+          path="/boards"
+          element={
+            <PrivateRoute>
+              <p>Boards</p>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/boards/:boardId"
+          element={
+            <PrivateRoute>
+              <p>Board ID</p>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <p>profile</p>
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<p>404</p>} />
+      </Routes>
     </div>
   );
 }
