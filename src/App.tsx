b48f64feb9a8ea6cd575be5fa47613d95636a15a
rincewizz <95530503+rincewizz@users.noreply.github.com>
2022-11-12 14:09:06 +0300

    RSS-5: add redux store (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import PrivateRoute from 'components/routing/PrivateRoute';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { Container } from 'react-bootstrap';
 import './i18n/config';
 import { useTranslation } from 'react-i18next';
+import { Boards } from 'views/boards';
+import { store } from 'store/store';
 import Footer from 'components/layout/footer/Footer';
 
 export default function App() {
@@ -14,46 +17,49 @@ export default function App() {
   };
 
   return (
-    <div className="min-vh-100 d-flex flex-column">
-      <header className="App-header">
-        <Container className="text-center">
-          <h1 onClick={() => changeLanguage('ru')}>Start project.</h1>
-          <p>{t('title')}</p>
-        </Container>
-      </header>
-      <main className="flex-grow-1 px-3">
-        <Routes>
-          <Route path="/" element={<p>Welcome page</p>} />
-          <Route path="/login" element={<p>Sign in</p>} />
-          <Route path="/registration" element={<p>Sign up</p>} />
-          <Route
-            path="/boards"
-            element={
-              <PrivateRoute>
-                <p>Boards</p>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/boards/:boardId"
-            element={
-              <PrivateRoute>
-                <p>Board ID</p>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <PrivateRoute>
-                <p>profile</p>
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<p>404</p>} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className="min-vh-100 d-flex flex-column">
+        <header className="App-header">
+          <Container className="text-center">
+            <h1 onClick={() => changeLanguage('ru')}>Start project.</h1>
+            <p>{t('title')}</p>
+          </Container>
+        </header>
+        <main className="flex-grow-1 px-3">
+          <Routes>
+            <Route path="/" element={<p>Welcome page</p>} />
+            <Route path="/login" element={<p>Sign in</p>} />
+            <Route path="/registration" element={<p>Sign up</p>} />
+            <Route path="/brds" element={<Boards />} />
+            <Route
+              path="/boards"
+              element={
+                <PrivateRoute>
+                  <p>Boards</p>
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/boards/:boardId"
+              element={
+                <PrivateRoute>
+                  <p>Board ID</p>
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <p>profile</p>
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<p>404</p>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Provider>
   );
 }
---
+++ src/store/authSlice.ts
@@ -0,0 +1,70 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from './store';
+import { AuthState } from './types';
+
+const initialState: AuthState = { auth: {}, status: 'idle', error: null };
+
+export const signin = createAsyncThunk(
+  'auth/signin',
+  async ({ login, password }: { login: string; password: string }, thunkAPI) => {
+    // const response = await api.signin();
+    // const response = await api.getUserById();
+    return { _id: 'qwerty', name: 'IMask', login: 'IMask', token: '123456', exp: 12345678 };
+  }
+);
+export const signup = createAsyncThunk(
+  'auth/signup',
+  async (
+    { name, login, password }: { name: string; login: string; password: string },
+    thunkAPI
+  ) => {
+    // const response = await api.signup();
+    return { _id: 'qwerty', name: 'IMask', login: 'IMask' };
+  }
+);
+
+export const fetchUserById = createAsyncThunk(
+  'auth/fetchUserById',
+  async ({ userId }: { userId: string }, thunkAPI) => {
+    // const response = await api.signup();
+    return { _id: 'qwerty', name: 'IMask', login: 'IMask' };
+  }
+);
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(signin.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(signin.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.auth = action.payload;
+      })
+      .addCase(signin.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message ?? '';
+      })
+
+      .addCase(signup.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(signup.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.auth = action.payload;
+      })
+      .addCase(signup.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message ?? '';
+      });
+  },
+});
+
+export const {} = authSlice.actions;
+
+export const selectAuth = (state: RootState) => state.auth;
+
+export default authSlice.reducer;
