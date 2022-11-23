89cee6fd8f2073a02e1f19ea2c841aa79e174b9e
Ol4ik88 <slapikol4ik@gmail.com>
2022-11-23 15:57:20 +0300

    RSS-30: create toast component

---
+++ src/components/pushMessage/PushMessage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Toast from 'react-bootstrap/Toast';
+import { IPushMessage } from './PushMessage.type';
+
+function PushMessage({ text, isShow, onHide }: IPushMessage) {
+  return (
+    <Toast
+      className="position-fixed bottom-0 end-0 border border-2 border-warning"
+      onClose={onHide}
+      show={isShow}
+      delay={3000}
+      autohide
+    >
+      <Toast.Header className="justify-content-between text-dark">{text}</Toast.Header>
+    </Toast>
+  );
+}
+
+export default PushMessage;
