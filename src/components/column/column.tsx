d58ca968d359f2291be6e4f2309a819e1c27049d
Ol4ik88 <slapikol4ik@gmail.com>
2022-11-25 16:30:16 +0300

    RSS-40: Implement column header interactivity

--- src/components/column/column.tsx
+++ src/components/column/column.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import React, { useEffect, useState, ChangeEvent } from 'react';
+import { Button, Card, Form, Col } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from 'store/store';
 import edit_icon from '../../assets/registaration_icon.svg';
+import cancel_icon from '../../assets/cancel.svg';
 import delete_icon from '../../assets/delete_icon.svg';
 import { Task } from 'components/task/task';
 import ModalWindow from 'components/modal/ModalWindow';
@@ -13,6 +14,7 @@ import { CreateTaskForm } from 'components/forms/CreateTaskForm';
 import { DeleteWindow } from 'components/modal/DeleteWindow';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
+import { updateColumn } from 'store/columnSlice';
 
 export const Column = ({ column, isDragging }: { column: IColumn; isDragging?: boolean }) => {
   const dispatch = useDispatch<AppDispatch>();
@@ -21,6 +23,8 @@ export const Column = ({ column, isDragging }: { column: IColumn; isDragging?: b
   const [modalContent, setModalContent] = useState<React.ReactNode>();
   const [modalTitle, setModalTitle] = useState<string>('');
   const [isOpen, setIsOpen] = useState(false);
+  const [isInput, setIsInput] = useState(false);
+  const [inputTitle, setInputTitle] = useState(title);
 
   const {
     entities: tasksEntities,
@@ -43,22 +47,35 @@ export const Column = ({ column, isDragging }: { column: IColumn; isDragging?: b
   };
 
   const handleClickDelete = () => {
-    setModalTitle(t('board.remove task title') ?? '');
+    setModalTitle(t('board.remove column title') ?? '');
     setModalContent(
       <DeleteWindow
         cancel={onHide}
         remove={() => {
-          alert('удаление задачи');
+          alert('удаление столбца');
         }}
-        text={t('board.remove task message')}
+        text={t('board.remove column message')}
       />
     );
     setIsOpen(true);
   };
 
   const handleClickEdit = () => {
-    alert('изменить название столбца');
+    dispatch(updateColumn({ boardId, columnId: _id, title: inputTitle, order }));
+    setIsInput(false);
   };
+
+  const handleClickCancell = () => {
+    setIsInput(false);
+  };
+
+  const handleClickTitle = () => {
+    setIsInput(true);
+  };
+
+  function handlerChangeTitle(e: ChangeEvent<HTMLInputElement>) {
+    setInputTitle(e.target.value);
+  }
 
   const handleClickCreate = () => {
     setModalTitle(t('board.create task') ?? '');
@@ -77,15 +94,38 @@ export const Column = ({ column, isDragging }: { column: IColumn; isDragging?: b
         style={{ ...style, width: '272px' }}
         id={column._id}
       >
-        <Card.Header ref={setActivatorNodeRef} {...listeners}>
-          {title}
-          <Button variant="light" className="col-3 p-0" size="sm" onClick={handleClickEdit}>
-            <img width="20" src={edit_icon} alt="edit" />
-          </Button>
-
-          <Button variant="light" className="col-3 p-0" size="sm" onClick={handleClickDelete}>
-            <img width="20" src={delete_icon} alt="delete" />
-          </Button>
+        <Card.Header
+          ref={setActivatorNodeRef}
+          {...listeners}
+          className="d-flex justify-content-between"
+        >
+          {!isInput ? (
+            <>
+              <Col onClick={handleClickTitle}>{title}</Col>
+              <Button variant="light" className="p-0" size="sm" onClick={handleClickDelete}>
+                <img width="20" src={delete_icon} alt="delete" />
+              </Button>
+            </>
+          ) : (
+            <Form onSubmit={handleClickEdit} className="row">
+              <Col className="col-8 p-0">
+                <Form.Control
+                  required
+                  type="text"
+                  value={inputTitle}
+                  onChange={handlerChangeTitle}
+                />
+              </Col>
+              <Col className="col-4 px-1">
+                <Button variant="light" type="submit" size="sm">
+                  <img width="15" src={edit_icon} alt="edit" />
+                </Button>
+                <Button variant="light" type="reset" size="sm" onClick={handleClickCancell}>
+                  <img width="15" src={cancel_icon} alt="cancel" />
+                </Button>
+              </Col>
+            </Form>
+          )}
         </Card.Header>
 
         <Card.Body className="p-1">
