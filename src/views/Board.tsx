1c3bedf09373261d19e3242a50f17bf8b7283254
yolf <gleoros@gmail.com>
2022-11-30 19:26:31 +0300

    RSS-47: disable access to api when visiting non-existent board

--- src/views/Board.tsx
+++ src/views/Board.tsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { boardsSelectors, fetchBoardById, selectBoards } from 'store/boardsSlice';
 import { AppDispatch } from 'store/store';
+import { useAuthStatus } from 'utils/helpers/authHelper';
 import './Board.css';
 
 export function Board() {
@@ -14,32 +15,38 @@ export function Board() {
   const { entities, error, status, statuses } = useSelector(selectBoards);
   const { t } = useTranslation();
   const { boardId } = useParams();
+  const { isAuth } = useAuthStatus();
 
   useEffect(() => {
-    if (boardId && !statuses[boardId] && status !== 'succeeded') {
+    if (boardId && !statuses[boardId] && status !== 'succeeded' && isAuth()) {
       dispatch(fetchBoardById({ boardId }));
     }
   }, [dispatch]);
 
   return (
     <>
-      <Container fluid>
-        {status === 'failed' && (
-          <Alert className="text-center" variant={'danger'}>
-            {error}
-          </Alert>
-        )}
-        {boardId && statuses[boardId] === 'loading' && (
-          <Alert className="text-center" variant={'info'}>
-            {t('board.loading')}
-          </Alert>
-        )}
-
-        {boardId && entities[boardId] && <BoardTitle board={entities[boardId]} />}
-      </Container>
-      <Container fluid className="board">
-        {boardId && <BoardField boardId={boardId as string} />}
-      </Container>
+      {isAuth() && (
+        <>
+          <Container fluid>
+            {status === 'failed' && (
+              <Alert className="text-center" variant={'danger'}>
+                {error}
+              </Alert>
+            )}
+            {boardId && statuses[boardId] === 'loading' && status !== 'failed' && (
+              <Alert className="text-center" variant={'info'}>
+                {t('board.loading')}
+              </Alert>
+            )}
+          </Container>
+          {boardId && entities[boardId] && status !== 'failed' && (
+            <Container fluid className="board">
+              <BoardTitle board={entities[boardId]} />
+              <BoardField boardId={boardId as string} />
+            </Container>
+          )}
+        </>
+      )}
     </>
   );
 }
