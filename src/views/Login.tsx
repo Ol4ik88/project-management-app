3a4a90f42ed662ea52ee96f28493a125579fcec6
Yura <Lyurik@tut.by>
2022-11-30 16:16:05 +0300

    RSS-44-Banning-an-authorized-user-from-login-and-registration: add navigate to boards page (#52)

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,6 +1,15 @@
 import LoginForm from 'components/forms/LoginForm';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectAuth } from 'store/authSlice';
 
 export const Login = () => {
+  const { auth } = useSelector(selectAuth);
+
+  if (auth._id) {
+    return <Navigate to="/boards" />;
+  }
+
   return <LoginForm />;
 };
