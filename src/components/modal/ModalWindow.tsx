416cda5bffc9d10d309ba9bbb3a2a37e507433a2
Ol4ik88 <99606360+Ol4ik88@users.noreply.github.com>
2022-12-01 16:44:11 +0300

    Feature/RSS-59: add scroll to modal window (#57)
    * RSS-59: add scroll to modal window
    
    * RSS-59: delete calc

--- src/components/modal/ModalWindow.tsx
+++ src/components/modal/ModalWindow.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 import { Modal } from 'react-bootstrap';
+import './ModalWindow.css';
 
 function ModalWindow(props: {
   children: React.ReactNode;
@@ -13,7 +14,14 @@ function ModalWindow(props: {
   const portalContainer = document.body as HTMLElement;
 
   return createPortal(
-    <Modal show={show} onHide={onHide} aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal
+      show={show}
+      onHide={onHide}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      className="modal-height"
+      scrollable
+    >
       <Modal.Header closeButton>
         <Modal.Title>{modalTitle}</Modal.Title>
       </Modal.Header>
