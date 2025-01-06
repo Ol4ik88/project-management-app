b9a32a30694feca2b9ca9bf7bd12bbd88fe82a26
ol4ik88 <slapikol4ik@gmail.com>
2025-01-06 14:39:45 +0300

    Update README.md

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,2 +1,2 @@
-export const URL = 'http://localhost:4000/';
-// export const URL = 'https://kanban-api-6ca8.onrender.com/';
+//export const URL = 'http://localhost:4000/';
+export const URL = 'https://final-task-backend-production-f46a.up.railway.app/';
